d99ac1897f804bb570ab77043d52a7d9b2a353c9
Han <coc.hans@yahoo.com>
2024-08-28 20:02:33 +0700

    how to center a div?

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header: React.FC = () =>{
+    return(
+        // <header className="bg-white bg-opacity-70 backdrop-blur flex justify-center items-center p-4 h-screen">
+        <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center bg-white bg-opacity-70 backdrop-blur p-4">
+            <h1 className="text-3xl font-bold">NeuroCraft: Craft your own Neural Network, no code</h1>
+        </div>
+
+        // </header>
+
+    )
+}
+
+export default Header;
