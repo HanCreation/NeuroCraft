3957d72ba1ff47294024314bf64ded3ab6f775bf
Han <coc.hans@yahoo.com>
2024-08-28 20:20:42 +0700

    already centered a div

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -3,16 +3,14 @@ import React, { useState } from 'react'
 
 const form: React.FC = () => {
     return(
-        <>
-
-           <form className='max-w-sm mx-auto align-cente'>
-             <div className='mb-5 items-center'>
+        <div className="flex items-center justify-center h-screen">
+           <form className="p-8 bg-white shadow-lg">
+             <div className=''>
                 <label htmlFor='username' className='block text-gray-700 text-sm font-bold mb-2'>Username</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                
              </div>
-
             </form>
-        </>
+        </div>
 
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 const Header: React.FC = () =>{
     return(
+    
         // <header className="bg-white bg-opacity-70 backdrop-blur flex justify-center items-center p-4 h-screen">
-        <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center bg-white bg-opacity-70 backdrop-blur p-4">
-            <h1 className="text-3xl font-bold">NeuroCraft: Craft your own Neural Network, no code</h1>
+        <div >
+            <h1 className="text-3xl font-bold absolute top-0 left-0 w-full flex justify-center bg-opacity-70 p-4">NeuroCraft: Craft your own Neural Network, no code</h1>
         </div>
 
         // </header>
