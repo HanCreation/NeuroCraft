fc8688e68742f93d46e5410ba5d3bc05564f8d30
Han <coc.hans@yahoo.com>
2024-08-31 09:59:13 +0700

    Update form.tsx

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
+
 const form: React.FC = () => {
+  const [dataset, setDataset] = useState("mnist");
+
+
   return (
     <div className="flex h-screen items-center justify-center">
       <form className="rounded-lg bg-white p-8 shadow-lg">
@@ -21,11 +25,18 @@ const form: React.FC = () => {
                 className="flex h-9 w-10 appearance-none items-center justify-center rounded-sm bg-[#1f1f1f]"
                 name="size"
                 type="radio"
-                value="xs"
+                value="mnist"
                 checked
               ></input>
+              {/* <div className="absolute"> */}
+              <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/2/27/MnistExamples.png"
+                  alt="mnist"
+                  className="h-20 w-20"
+                ></img>
+              {/* </div> */}
               <span className="absolute inset-0 flex items-center justify-center text-white">
-                XS
+                MNIST
               </span>
             </label>
             <div>
